390e819cdf62d19670e5a7830bed18672d6e7bb9
Pavel <Filosof1602@mail.ru>
2022-09-26 12:17:36 +0400

    дз№3

--- js/main.js
+++ js/main.js
@@ -46,10 +46,6 @@ const COMENTS = [
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
   'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
   'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
-  '',
-  '',
-  '',
-  '',
 ];
 
 const NAMES = [
@@ -57,39 +53,43 @@ const NAMES = [
   'piter',
   'dasha',
   'max',
-]
+];
 
-const countObj = 25;
-const randomArrayId = generateArrayRandomNumber(1, countObj);
-const randomArrayUrl = generateArrayRandomNumber(1, countObj);
+const basePhotos = [];
+const COUNT_OBJ = 25;
+const randomArrayId = generateArrayRandomNumber(1, COUNT_OBJ);
+const randomArrayUrl = generateArrayRandomNumber(1, COUNT_OBJ);
+const comments = { min: 0, max: 3 };
+const id = { min: 1, max: 999 };
+const avatar = { min: 1, max: 6 };
+const likes = { min: 15, max: 200 };
 
-const createObj = (i) => {
-  return {
-    id: randomArrayId[i],
-    url: 'photos/' + randomArrayUrl[i] + '.jpg',
-    description: DESCRIPTIONS[getRandomNumber(0, DESCRIPTIONS.length)],
-    likes: getRandomNumber(15, 200),
-    comment: {
-      id: '',
-      avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
-      message: COMENTS[getRandomNumber(0, 5)] + ' ' + COMENTS[getRandomNumber(0, COMENTS.length)],
+const addComments = () => {
+  let comentsBase = [];
+  for (let i = 0; i <= getRandomNumber(comments.min, comments.max); i++) {
+    comentsBase.push({
+      id: getRandomNumber(id.min, id.max),
+      avatar: 'img/avatar-' + getRandomNumber(avatar.min, avatar.max) + '.svg',
+      message: COMENTS[getRandomNumber(0, COMENTS.length)],
       name: NAMES[getRandomNumber(0, NAMES.length)],
-    },
-    comment_1: {
-      id: '',
-      avatar: 'img/avatar-' + getRandomNumber(1, 6) + '.svg',
-      message: COMENTS[getRandomNumber(0, 5)] + ' ' + COMENTS[getRandomNumber(0, COMENTS.length)],
-      name: NAMES[getRandomNumber(0, NAMES.length)],
-    },
+    });
   }
-}
-
-const buildBase = () => {
-  let base = []
-  for (let j = 0; j < countObj; j++) {
-    base.push(createObj(j));
-  }
-  return base;
+  return comentsBase;
 };
 
-console.log(buildBase());
+
+const createObj = () => {
+  for (let i = 0; i < COUNT_OBJ; i++) {
+    basePhotos.push({
+      id: randomArrayId[i],
+      url: 'photos/' + randomArrayUrl[i] + '.jpg',
+      description: DESCRIPTIONS[getRandomNumber(0, DESCRIPTIONS.length)],
+      likes: getRandomNumber(likes.min, likes.max),
+      comment: addComments(),
+    })
+  }
+};
+
+createObj();
+
+console.log(basePhotos);
